d3faf125c148e5b75e9c2a6d3f50c6f71b49209e
mahasulaiman <mhao_h@hotmail.com>
2021-12-24 16:30:39 +0300

    add js

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ const timerCount = () => {
     const min = Math.floor(time / 60);
     const sec = time % 60;
     if (sec < 10){
-        timer.innerHTML = '${min}:0${sec}';
+        timer.innerHTML = `${min}:0${sec}`;
     } else {
-        timer.innerHTML = '${min}:${sec}';
+        timer.innerHTML = `${min}:${sec}`;
     }
 };
 const stopClock = () => {
